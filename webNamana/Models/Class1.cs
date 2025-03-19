08479e7ede33a35f5b07004419af4861fbfabda6
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-03-19 14:38:14 +0200

    models

---
+++ webNamana/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webNamana.Models
+{
+	public class Class1
+	{
+		public string Username { get; set; }
+		public List<string> Products { get; set; }
+
+    }
+}
