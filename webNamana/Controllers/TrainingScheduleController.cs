70f4b86853f0d8e7a412ce4a67b3d0b7c58b8746
wwwndsr <cristina.perepeliuc@isa.utm.md>
2025-04-26 19:29:42 +0300

    добавление страницы покупки абнемента, расписание тренировок, измененная версия странцы регистрации на тренировку, измененный вид страницы входа в аккаунт и регистрации нового пользвателя

---
+++ webNamana/Controllers/TrainingScheduleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webNamana.Controllers
+{
+    public class TrainingScheduleController : Controller
+    {
+        // GET: TrainingSchedule
+        public ActionResult TrainingSchedule()
+        {
+            return View();
+        }
+    }
+}
